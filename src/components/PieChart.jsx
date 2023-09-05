fc21bc9b6371111cc597574a8050250ee25ae628
Pierre <pierrelouis.abraham@gmail.com>
2023-09-05 14:56:32 +0200

    push style + data

--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { RadarChart, PolarGrid, PolarAngleAxis, Radar, ResponsiveContainer, RadialBarChart, RadialBar, Legend } from 'recharts';
 
-const TodaysScoreRadarChart = (todayScoreData) => {
+const TodaysScoreRadarChart = ({ todayScoreData }) => {
   const res = [{ score: todayScoreData }]
-  console.log(todayScoreData)
+  console.log(todayScoreData, JSON.stringify(res))
 
   const RenderLegend = () => (
     <div className="score-container">
@@ -12,40 +12,40 @@ const TodaysScoreRadarChart = (todayScoreData) => {
     </div>
   )
 
- 
+
 
   return (
-    <>
-    <ResponsiveContainer height={263}>
-      <RadialBarChart
-        cx="50%"
-        cy="50%"
-        innerRadius="80%"
-        outerRadius="90%"
-        barSize={15}
-        data={res}
-        startAngle={90}
-        endAngle={450}
-      >
-        <PolarAngleAxis type="number" domain={[0, 1]} tick={false} />
-        <RadialBar
-          minAngle={15}
-          clockWise
-          dataKey="score"
-          fill="red"
-          cornerRadius={20}
-        />
-        <Legend
-          width={120}
-          height={120}
-          layout="vertical"
-          verticalAlign="middle"
-          align="center"
-          content={<RenderLegend />}
-        />
-      </RadialBarChart>
-    </ResponsiveContainer>
-    </>
+    <div className='radial-bar'>  
+      <ResponsiveContainer height={263}>
+        <RadialBarChart
+          cx="50%"
+          cy="50%"
+          innerRadius="80%"
+          outerRadius="90%"
+          barSize={15}
+          data={res}
+          startAngle={90}
+          endAngle={450}
+        >
+          <PolarAngleAxis type="number" domain={[0, 1]} tick={false} />
+          <RadialBar
+            minAngle={15}
+            clockWise
+            dataKey="score"
+            fill="red"
+            cornerRadius={20}
+          />
+          <Legend
+            width={120}
+            height={120}
+            layout="vertical"
+            verticalAlign="middle"
+            align="center"
+            content={<RenderLegend />}
+          />
+        </RadialBarChart>
+      </ResponsiveContainer>
+      </div>
   );
 };
 
